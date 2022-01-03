306da7dcbcd3dfa635ca33e662972cb8882a5e40
harry-sar <mail@harrysar.dev>
2022-01-03 20:12:18 +0000

    Fixed Finding All 8 neighbours

--- A*Search.py
+++ A*Search.py
@@ -11,8 +11,18 @@ class aStarSearch:
         self.graph_init = {}
         self.graph = {}
 
+        count_gen=1
         for x in range(self.grid_size ** 2):
-            self.graph_init[x + 1] = "[-]"
+            if count_gen==1:
+                self.graph_init[x + 1] = "START"
+            if x+1<=self.grid_size:
+                self.graph_init[x + 1] = "TOP"
+            if count_gen==self.grid_size:
+                self.graph_init[x+1]="END"
+                count_gen=0
+            elif count_gen!=1 and count_gen!=self.grid_size and not x+1<=self.grid_size:
+                self.graph_init[x + 1] = "[-]"
+            count_gen+=1
 
         self.open_dict = {}  # cell: cost
         self.closed_dict = {}
@@ -36,19 +46,19 @@ class aStarSearch:
             hasUp = None
         # Checking if UR cell
         try:
-            if self.graph_init[cell - self.grid_size + 1]:
+            if self.graph_init[cell - self.grid_size + 1] and self.graph_init[cell] not in ["TOP","END"]:
                 hasBtwnUR = cell - self.grid_size + 1
         except:
             hasBtwnUR = None
         # Checking if Right cell
         try:
-            if self.graph_init[cell + 1]:
+            if self.graph_init[cell + 1] and self.graph_init[cell] not in ["END"]:
                 hasRight = cell + 1
         except:
             hasRight = None
         # Checking if RD cell
         try:
-            if self.graph_init[cell + self.grid_size + 1]:
+            if self.graph_init[cell + self.grid_size + 1] and self.graph_init[cell] not in ["END"]:
                 hasBtwnRD = cell + self.grid_size + 1
         except:
             hasBtwnRD = None
@@ -60,19 +70,19 @@ class aStarSearch:
             hasDown = None
         # Checking if LD cell
         try:
-            if self.graph_init[cell + self.grid_size - 1] and ((cell+self.grid_size-1)%self.grid_size!=0):
+            if self.graph_init[cell + self.grid_size - 1] and self.graph_init[cell] not in ["START"] and cell!=1:
                 hasBtwnLD = cell + self.grid_size - 1
         except:
             hasBtwnLD = None
         # Checking if Left cell
         try:
-            if self.graph_init[cell - 1]:
+            if self.graph_init[cell - 1] and self.graph_init[cell] not in ["START"] and cell!=1:
                 hasLeft = cell - 1
         except:
             hasLeft = None
         # Checking if LU cell
         try:
-            if self.graph_init[cell - self.grid_size - 1]:
+            if self.graph_init[cell - self.grid_size - 1] and self.graph_init[cell] not in ["START","TOP"]:
                 hasBtwnLU = cell - self.grid_size - 1
         except:
             hasBtwnLU = None
@@ -84,8 +94,8 @@ class aStarSearch:
 
 
 test = aStarSearch(5)
-# test.print_grid()
+test.print_grid()
 
-lol=[test.check_8_neighbours(5)]
+# lol=[test.check_8_neighbours(5)]
 
 print(lol)
